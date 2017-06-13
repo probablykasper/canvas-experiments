a3d7ea52954698badc33293ff011e09d85fa2c11
KH <kasperkh.kh@gmail.com>
2017-06-13 22:03:36 +0200

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="theme-color" content="#db5945">
+		<title>Canvas</title>
+		<!--Roboto--> <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
+		<!--Favicon: http:/#F986DF/realfavicongenerator.net -->
+		<link rel="stylesheet" type="text/css" href="/css/global.css?r=<?=rand(0,999)?>">
+		<link rel="stylesheet" type="text/css" href="/css/home.css?r=<?=rand(0,999)?>">
+	</head>
+
+	<body>
+		<canvas width="200px" height="100px"></canvas>
+		<!--jQuery--> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<!--js.cookie--> <script src="/js/js.cookie-2.1.4.min.js"></script>
+	</body>
+</html>
