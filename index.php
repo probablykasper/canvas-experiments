b9c015ef492a347d78c1635c8f171563fbecdf98
KH <kasperkh.kh@gmail.com>
2017-06-14 20:33:55 +0200

    Drawing rectangles, line & randomly positioned circles

--- index.php
+++ index.php
@@ -6,16 +6,16 @@
 		<title>Canvas</title>
 		<!--Roboto--> <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
 		<!--Favicon: http:/#F986DF/realfavicongenerator.net -->
-		<link rel="stylesheet" type="text/css" href="/css/global.css?r=<?=rand(0,999)?>">
-		<link rel="stylesheet" type="text/css" href="/css/home.css?r=<?=rand(0,999)?>">
+		<link rel="stylesheet" type="text/css" href="/css/global.css<?="?r=".rand(0,999)?>">
+		<link rel="stylesheet" type="text/css" href="/css/home.css<?="?r=".rand(0,999)?>">
 	</head>
 
 	<body>
 		<section class="main">
-			<canvas id="canvas"></canvas>
+			<canvas width="300px" height="150px"></canvas>
 		</section>
 		<!--jQuery--> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<!--js.cookie--> <script src="/js/js.cookie-2.1.4.min.js"></script>
-		<script src="js/home.js"></script>
+		<script src="js/home.js<?="?r=".rand(0,999)?>"></script>
 	</body>
 </html>
--- js/home.js
+++ js/home.js
@@ -1,19 +1,51 @@
-window.onload = draw;
+// window.onload = draw;
+var canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth-20;
+canvas.height = window.innerHeight-20;
+var c = canvas.getContext("2d");
 
-function draw() {
-	// Assign canvas el to var
-	var c = document.getElementById("canvas");
-	var ctx = c.getContext("2d");
-	var ch = c.clientHeight;
-	var cw = c.clientWidth;
+// c.fillRect(x, y, w, h);
+c.fillStyle = "#00C8A4";
+c.fillRect(100, 100, 100, 100);
 
-	// fillStyle(r,g,b,alpha)
-	// fillRect(x,y,w,h)
+c.fillStyle = "#0086C8";
+c.fillRect(200, 200, 100, 100);
+c.fillStyle = "#C8005A";
+c.fillRect(300, 300, 100, 100);
 
-	ctx.fillStyle = "rgba(0, 200, 164, 1)";
-	var gap = 2;
-	ctx.fillRect(cw/2-25-gap, ch/2-25-gap, 50, 50);
+c.beginPath();
+// c.moveTo(x,y);
+c.moveTo(200, 100);
+c.lineTo(400, 300);
+c.strokeStyle = "#000000";
+c.stroke();
+c.fillRect(200, 100, 100, -100);
+c.fillRect(300, 200, 100, -100);
+c.fillRect(400, 300, 100, -100);
 
-	ctx.fillStyle = "rgba(0, 134, 200, 1)";
-	ctx.fillRect(cw/2-25+gap, ch/2-25+gap, 50, 50);
+c.beginPath();
+// c.arc(x, y, radius, startAngle, endAngle, drawCounterClockwise?)
+c.strokeStyle = "#00C8A4";
+// c.arc(250, 150, 50, 0, Math.PI*2, false);
+c.stroke();
+
+for (var i = 0; i < 10; i++) {
+	var x = Math.random() * canvas.width;
+	var y = Math.random() * canvas.height;
+	switch ( Math.ceil( Math.random()*3 ) ) {
+		case 1:
+		c.strokeStyle = "#00C8A4";
+		break;
+		case 2:
+		c.strokeStyle = "#dddddd";
+		break;
+		case 3:
+		c.strokeStyle = "#000000";
+		break;
+	}
+	c.lineWidth = 2;
+	console.log(i);
+	c.beginPath();
+	c.arc(x, y, 50, 0, Math.PI*2, false);
+	c.stroke();
 }
