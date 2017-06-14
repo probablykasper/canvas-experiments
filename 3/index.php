5bc4c86e59ae2e913cb6bf3050cfff28c969b330
KH <kasperkh.kh@gmail.com>
2017-06-14 21:33:35 +0200

    fix (woops everything broke)

--- 3/index.php
+++ 3/index.php
@@ -6,14 +6,14 @@
 		<title>Canvas</title>
 		<!--Roboto--> <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
 		<!--Favicon: http:/#F986DF/realfavicongenerator.net -->
-		<link rel="stylesheet" type="text/css" href="/css/home.css?r=<?=rand(0,999)?>">
+		<link rel="stylesheet" type="text/css" href="../css/home.css?r=<?=rand(0,999)?>">
 	</head>
 
 	<body>
 		<section class="main">
 			<canvas></canvas>
 		</section>
-		<!--jQuery--> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<script src="/js/home.js?r=<?rand(0,999)?>"></script>
+		<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+		<script src="main.js"></script>
 	</body>
 </html>
