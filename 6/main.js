7e19ba77d7cf179b17181605e7aacfd2c2c6dac1
KH <kasperkh.kh@gmail.com>
2017-06-17 00:12:27 +0200

    Added comments

--- 6/main.js
+++ 6/main.js
@@ -1,81 +1,3 @@
-// function Circle(x, y, dx, dy, radius, color) {
-// 	this.radius = radius;
-// 	this.x = x;
-// 	this.y = y;
-// 	this.dx = dx;
-// 	this.dy = dy;
-// 	this.color = color;
-//
-// 	this.draw = function() {
-// 		// draw circle
-// 		c.beginPath();
-// 		c.fillStyle = this.color;
-// 		c.lineWidth = 2;
-// 		c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false); //draw the actual circle
-// 		c.fill();
-// 	}
-// 	this.update = function(currentCircleIndex) {
-//
-// 		for (var i = 0; i < newCircles.length; i++) {
-//
-//             if (i != currentCircleIndex) {
-//                 var distancex = newCircles[i].x - this.x;
-//                 var distancey = newCircles[i].y - this.y;
-//                 var distanceBetweenCircles = Math.sqrt( distancex*distancex + distancey*distancey );
-//                 // console.log(distancex + "   " + newCircles[i].x + "   " + this.x + "   " + i);
-//                 // cat^2 + cat^2 = hyp^2
-//
-//                 var distance = this.radius + newCircles[i].radius;
-//                 var directionx = (newCircles[i].x - this.x) / distance;
-//                 var directiony = (newCircles[i].y - this.y) / distance;
-//                 var intersectionx = this.x + directionx * this.radius;
-//                 var intersectiony = this.y + directiony * this.radius;
-//
-//                 // dir - 2 * dot(dir, normal) * normal
-//
-//                 if (distanceBetweenCircles < 0) distanceBetweenCircles = -distanceBetweenCircles;
-//                 if (distanceBetweenCircles < distance) {
-//
-//                     var normalx = (intersectionx - this.x) / this.radius;
-//                     var normaly = (intersectiony - this.y) / this.radius;
-//                     var newDirx = directionx - 2 * (  directionx * normalx + directiony * normaly  ) * normalx;
-//                     var newDiry = directiony - 2 * (  directionx * normalx + directiony * normaly  ) * normaly;
-//
-//                     this.dx = newDirx;
-//                     this.dy = newDiry;
-//
-//                     // this.dx = -directionx+(this.dx+this.dy)/2;
-//                     // this.dy = -directiony+(this.dx+this.dy)/2;
-//                 }
-//             }
-//
-// 		}
-//
-// 		// bounce on edges
-// 		if (this.x >= canvas.width-this.radius) this.dx = -this.dx;
-// 		if (this.x <= this.radius) this.dx = -this.dx;
-// 		if (this.y >= canvas.height-this.radius) this.dy = -this.dy;
-// 		if (this.y <= this.radius) this.dy = -this.dy;
-// 		// if (this.x >= canvas.width-this.radius) this.dx = -(Math.random()+1)*3;
-// 		// if (this.x <= this.radius) this.dx = (Math.random()+1)*3;
-// 		// if (this.y >= canvas.height-this.radius) this.dy = -(Math.random()+1)*3;
-// 		// if (this.y <= this.radius) this.dy = (Math.random()+1)*3;
-//
-// 		// this.dx = 0;
-// 		// this.dy = 0;
-//
-// 		this.x += this.dx;
-// 		this.y += this.dy;
-// 		this.draw();
-// 	}
-// }
-
-
-
-
-
-
-
 // init canvas
 var canvas = document.querySelector("canvas");
 var c = canvas.getContext("2d");
@@ -125,62 +47,20 @@ for (var i = 0; i < circleAmount; i++) {
 }
 
 function update(ci) { // circleIndex
-    // var radius = circles[ci].radius;
-	// var x = circles[ci].x;
-	// var y = circles[ci].y;
-	// var dx = circles[ci].dx;
-	// var dy = circles[ci].dy;
-    // var color = circles[ci].color;
-
-    // 		for (var i = 0; i < newCircles.length; i++) {
-    //
-                // if (i != currentCircleIndex) {
-                //     var distancex = newCircles[i].x - this.x;
-                //     var distancey = newCircles[i].y - this.y;
-                //     var distanceBetweenCircles = Math.sqrt( distancex*distancex + distancey*distancey );
-                //     // console.log(distancex + "   " + newCircles[i].x + "   " + this.x + "   " + i);
-                //     // cat^2 + cat^2 = hyp^2
-                //
-                //     var distance = this.radius + newCircles[i].radius;
-                //     var directionx = (newCircles[i].x - this.x) / distance;
-                //     var directiony = (newCircles[i].y - this.y) / distance;
-                //     var intersectionx = this.x + directionx * this.radius;
-                //     var intersectiony = this.y + directiony * this.radius;
-                //
-                //     // dir - 2 * dot(dir, normal) * normal
-                //
-                //     if (distanceBetweenCircles < 0) distanceBetweenCircles = -distanceBetweenCircles;
-                //     if (distanceBetweenCircles < distance) {
-                //
-                //         var normalx = (intersectionx - this.x) / this.radius;
-                //         var normaly = (intersectiony - this.y) / this.radius;
-                //         var newDirx = directionx - 2 * (  directionx * normalx + directiony * normaly  ) * normalx;
-                //         var newDiry = directiony - 2 * (  directionx * normalx + directiony * normaly  ) * normaly;
-                //
-                //         this.dx = newDirx;
-                //         this.dy = newDiry;
-                //
-                //         // this.dx = -directionx+(this.dx+this.dy)/2;
-                //         // this.dy = -directiony+(this.dx+this.dy)/2;
-                //     }
-                // }
-    //
-    // 		}
 
     for (var cpi = 0; cpi < circles.length; cpi++) { // comparisonIndex
-        if (cpi != ci) {
-            var xDist = circles[cpi].x - circles[ci].x;
-            var yDist = circles[cpi].y - circles[ci].y;
-            var distBetweenCircles = Math.sqrt( xDist*xDist + yDist*yDist );
-
-            var radii = circles[cpi].radius + circles[ci].radius;
-            var xDir = (circles[cpi].x - circles[ci].x) / radii;
-            var yDir = (circles[cpi].y - circles[ci].y) / radii;
-            var xInters = circles[ci].x + xDir * circles[ci].radius;
-            var yInters = circles[ci].y + yDir * circles[ci].radius;
-
+        if (cpi != ci) { // avoid comparing to itself
+            var xDist = circles[cpi].x - circles[ci].x; // difference of c1x & c2x
+            var yDist = circles[cpi].y - circles[ci].y; // difference of c1y & c2y
+            var distBetweenCircles = Math.sqrt( xDist*xDist + yDist*yDist ); // distance from c1 to c2
             if (distBetweenCircles < 0) distBetweenCircles = -distBetweenCircles;
-            if (distBetweenCircles < radii) {
+            var radii = circles[cpi].radius + circles[ci].radius; // the radii combined
+
+            if (distBetweenCircles < radii) { // if the circles are overlapping
+                var xDir = (circles[cpi].x - circles[ci].x) / radii;
+                var yDir = (circles[cpi].y - circles[ci].y) / radii;
+                var xInters = circles[ci].x + xDir * circles[ci].radius;
+                var yInters = circles[ci].y + yDir * circles[ci].radius;
                 var xNormal = (xInters - circles[ci].x) / circles[ci].radius;
                 var yNormal = (yInters - circles[ci].y) / circles[ci].radius;
                 var xNewDir = xDir - 2 * (  xDir * xNormal + yDir * yNormal  ) * xNormal;
@@ -192,30 +72,25 @@ function update(ci) { // circleIndex
         }
     }
 
+    // when they hit the walls
     if (nc[ci].x+nc[ci].radius < 0            ) nc[ci].x = canvas.width  + nc[ci].radius; // left
     if (nc[ci].x-nc[ci].radius > canvas.width ) nc[ci].x = 0             - nc[ci].radius; // right
     if (nc[ci].y+nc[ci].radius < 0            ) nc[ci].y = canvas.height + nc[ci].radius; // top
     if (nc[ci].y-nc[ci].radius > canvas.height) nc[ci].y = 0             - nc[ci].radius; // bottom
 
-    nc[ci].x += nc[ci].dx;
-    nc[ci].y += nc[ci].dy;
+    nc[ci].x += nc[ci].dx; // increment x position
+    nc[ci].y += nc[ci].dy; // increment y position
 
-    draw(nc[ci].radius, nc[ci].x, nc[ci].y, nc[ci].color);
+    draw(nc[ci].radius, nc[ci].x, nc[ci].y, nc[ci].color); // finally draw the circle
 }
 function animate() {
     requestAnimationFrame(animate); // init animation
     c.clearRect(0, 0, innerWidth, innerHeight); // clear canvas
 
     nc = circles;
-    for (var i = 0; i < circles.length; i++) {
+    for (var i = 0; i < circles.length; i++) { // update every circle
         update(i);
     }
     circles = nc;
 }
 animate();
-
-// var newCircles = [];
-// for (var i = 0; i < circleAmount; i++) {
-// 	newCircles.push(new Circle(x, y, dx, dy, radius, color));
-// 	newCircles[i].draw();
-// }
